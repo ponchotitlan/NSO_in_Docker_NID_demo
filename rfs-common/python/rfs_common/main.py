567eda732959d089e95ce0f0f3bfa2c67fd6bd78
ponchotitlan <alfsando@cisco.com>
2023-11-09 23:45:48 +0000

    NID skeleton demo

---
+++ rfs-common/python/rfs_common/main.py
@@ -0,0 +1,16 @@
+# -*- mode: python; python-indent: 4 -*-
+import ncs
+from ncs.application import Service
+
+
+class Main(ncs.application.Application):
+    def setup(self):
+        # The application class sets up logging for us. It is accessible
+        # through 'self.log' and is a ncs.log.Log instance.
+        self.log.info('Main RUNNING')
+
+    def teardown(self):
+        # When the application is finished (which would happen if NCS went
+        # down, packages were reloaded or some error occurred) this teardown
+        # method will be called.
+        self.log.info('Main FINISHED')
